525b32cb9dfeaee39e337d63baf44b9b02588bdf
Matthew1898 <56174767+Matthew1898@users.noreply.github.com>
2020-02-19 11:22:12 -0800

    characters

---
+++ Assets/Scripts/Forwrard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Forwrard : MonoBehaviour
+{
+    public float speed =50.0f;
+    public float forwardinput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        forwardinput = Input.GetAxis("Vertical");
+        transform.position =(Vector3.forward * forwardinput * Time.deltaTime * speed);
+    }
+}
